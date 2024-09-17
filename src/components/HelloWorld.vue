c4458023b8173dc3419c834b76827c0bc63ca54e
ReceiverBlank <ivan.t.vanya60@gmail.com>
2024-09-17 11:46:04 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="read-the-docs">Я Ткачнеок Iван</h1>
+
+  <p class="read-the-docs">
+    Я делать лаба, устать
+  </p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
